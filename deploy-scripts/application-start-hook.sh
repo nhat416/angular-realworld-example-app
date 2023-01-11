3eb9a95e8c5e68ab8caf0910fbe0ccf2c2c304a9
Nhat Tran <nhatt416@gmail.com>
2023-01-10 19:22:02 -0500

    initialize aws codepipeline config files

---
+++ deploy-scripts/application-start-hook.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# set up the npm path
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# navigate to the application directory
+cd /home/ec2-user/angular-app
+
+ng serve
